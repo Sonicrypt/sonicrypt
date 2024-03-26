19926f8d00f33fe22a6889d360ec7312bed8ceb3
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-26 11:38:01 +0600

    Update WiFiManager setupWiFi function to accept WifiConfig object

--- src/WiFiManager.cpp
+++ src/WiFiManager.cpp
@@ -1,10 +1,13 @@
 #include <WiFi.h>
 #include "WiFiManager.h"
 
-const char* ssid = "Sumit";
-const char* password = "sumit625";
+const char* ssid;
+const char* password;
 
-void setupWiFi() {
+void setupWiFi(WifiConfig wifie_config) {
+  ssid = wifie_config.ssid;
+  password = wifie_config.password;
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
