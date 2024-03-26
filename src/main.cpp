25c90bf835114c8d43d9d064b5cdb90141c82571
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-26 11:07:16 +0600

    Add LED pin mode setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 #include "WiFiManager.h"
 #include "WebSocketClient.h"
+#include "Notify.h"
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(CONFIRM_LED_PIN, OUTPUT);
+  pinMode(FINALIZE_LED_PIN, OUTPUT);
+  pinMode(ERROR_LED_PIN, OUTPUT);
+
   setupWiFi();
   initWebSocket();
 }
