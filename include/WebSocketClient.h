90986abd9264a4a2e048aea5445b01302bdf1969
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-26 11:34:00 +0600

    Add Config parameter to initWebSocket function

--- include/WebSocketClient.h
+++ include/WebSocketClient.h
@@ -4,10 +4,12 @@
 #include <ArduinoWebsockets.h>
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
+#include "Config.h"
 
 using namespace websockets;
 
-void initWebSocket();
+void initWebSocket(Config config);
+void connect();
 void handleWebSocket();
 void parseSubId(String message);
 void parseTx(String message);
