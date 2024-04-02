f78aa6b58f60b7be2194b0cb15ab3dec9ff88fcd
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 12:11:49 +0600

    Update Notify.h to include IO.h and add new functions for BLE notifications

--- include/Notify.h
+++ include/Notify.h
@@ -1,10 +1,10 @@
 #ifndef NOTIFY_H
 #define NOTIFY_H
 
-#define CONFIRM_LED_PIN 11
-#define FINALIZE_LED_PIN 12
-#define ERROR_LED_PIN 13
+#include "IO.h"
 
+void notifyBLEOn();
+void notifyBLEConnected();
 void notifyConfirmation();
 void notifyError();
 void notifyFinalization();
