6c7f9b86bb875461ba6e3e122362a5f8f4e5d4e3
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 12:11:45 +0600

    Add IO.h header file with pin definitions

---
+++ include/IO.h
@@ -0,0 +1,15 @@
+#ifndef IO_H
+#define IO_H
+
+
+// Output pins
+#define BLE_LED_PIN 11
+#define CONFIRM_LED_PIN 12
+#define FINALIZE_LED_PIN 13
+#define ERROR_LED_PIN 14
+#define BUZZER_PIN 15
+
+// Input pins
+#define BLE_PUSH_BUTTON_PIN 16
+
+#endif
