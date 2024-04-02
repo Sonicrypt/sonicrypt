7830fe4bbc3bbb1a398f2ae8b5cd75be01dc0fbd
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-02 12:11:42 +0600

    Add BLEName constant and update function declarations

--- include/BLEManager.h
+++ include/BLEManager.h
@@ -7,11 +7,21 @@
 #include <BLEUtils.h>
 #include "Config.h"
 
+#define BLEName "Sonicrypt"
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define MESSAGE_CHARACTERISTIC_UUID "6d68efe5-04b6-4a85-abc4-c2670b7bf7fd"
 
+const int MAX_PAYLOAD_SIZE = 20; // Maximum payload size in bytes
+const char delimiter = ';';
+const char startChar = '{';
+const char endChar = '}';
+const char keyValSplitChar = ':';
+
 void setupBLE();
+void disconnectBLE();
+void checkToReconnect();
 void sendText(String message);
-void setupConfig();
+bool  gotConfig();
+void setConfigComplete(bool value);
 
 #endif
